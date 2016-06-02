0de78adc733e612e153a5e725a4f1f17d83d4827
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-01 21:54:39 -0600

    Initial commit
    Pulled in day 1 for catch-of-the-day
    begin work on catch-of-the-day project, four components

---
+++ catch-of-the-day/01 - Introduction - Start Here/scripts/main.js
@@ -0,0 +1,118 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
+
+/*
+  App
+*/
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div class="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+});
+
+/*
+  Header
+*/
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+});
+
+/*
+  Order
+*/
+
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+  Inventory
+*/
+
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    // normal js comments
+    return (
+      <form className="store-selector">
+        {/* this is a jsx comment */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="Submit" />
+      </form>
+    )
+  }
+
+});
+
+/*
+  NotFound
+*/
+
+var NotFound = React.createClass({
+  render : function() {
+    return <h1>Not Found!</h1>
+  }
+});
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Router path="/" component={StorePicker}/>
+    <Router path="/store/:storeId" component={App}/>
+    <Router path="*" component={NotFound}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.getElementById('main'));
